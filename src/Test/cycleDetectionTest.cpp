088b490f5909a23bd8d8e558d66316b4d281a8a6
slimf1 <slimane.f41@gmail.com>
2022-01-31 11:14:26 +0100

    Cycle detection unit tests

--- src/Test/cycleDetectionTest.cpp
+++ src/Test/cycleDetectionTest.cpp
@@ -7,9 +7,47 @@
 TEST_CASE ( "Trivial function" ) {
 
     std::vector<int> vec = {1,2,3,4,5,6,4};
-    std::function<int(int)> f = [&](int a) {
+    std::function f = [&vec](int a) {
         return vec[a];
     };
 
-    REQUIRE ( aac::brent<int>(f, 0) ==  std::pair<int, int>(3,4) );
+    REQUIRE ( aac::brent(f, 0) ==  std::pair(3,4) );
+}
+
+TEST_CASE( "Collatz conjecture" ) {
+
+    std::function f = [](int a) {
+        if (a % 2 == 0) {
+            return a / 2;
+        }
+        else {
+            return 3 * a + 1;
+        }
+    };
+
+    for (int i = 0; i < 50; ++i) {
+        auto [length, outside] = aac::brent(f, i * 50 + 1);
+        REQUIRE(length == 3); // 4, 2, 1, ...
+    }
+}
+
+TEST_CASE( "One long cycle ") {
+
+    std::function f = [&](int a) {
+        return (a + 1) % 0xFFFA;
+    };
+
+    auto [length, outside] = aac::brent(f, 0);
+    REQUIRE(length == 0xFFFA);
+    REQUIRE(outside == 0); // Nothing not belonging to the cycle
+}
+
+TEST_CASE( "Instant cycle" ) {
+    std::function f = [&](int a) {
+        return 0;
+    };
+
+    auto [length, outside] = aac::brent(f, 0);
+    REQUIRE(length == 1);
+    REQUIRE(outside == 0);
 }
